f822886fb70aa0c283dc21e61f327cacd7e5de66
3LucasZ <lucas.zheng@warriorlife.net>
2025-05-01 22:50:04 -0700

    pan, zoom, clickable countires

--- webbenchdash/src/App.tsx
+++ webbenchdash/src/App.tsx
@@ -4,18 +4,22 @@ import { FeatureSelect } from "./components/macro/feature-select";
 import MapChart from "./components/macro/map-chart";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [selectedCountry, setSelectedCountry] = useState<string>("");
 
   return (
     <>
-      <h1 className="text-center text-6xl font-medium py-5">
+      <p className="text-center text-6xl font-medium py-5">
         WebBench Dashboard
-      </h1>
-      {/* <Button>Click me</Button> */}
+      </p>
       <div className="w-full flex justify-center">
         <FeatureSelect />
       </div>
-      <MapChart />
+      <div className="min-h-4"></div>
+      <p className="text-center text-3xl py-5">{selectedCountry}</p>
+      <MapChart
+        selectedCountry={selectedCountry}
+        setSelectedCountry={setSelectedCountry}
+      />
     </>
   );
 }
--- webbenchdash/src/components/macro/map-chart.tsx
+++ webbenchdash/src/components/macro/map-chart.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React, { MouseEventHandler, useEffect, useState } from "react";
 import { csv } from "d3-fetch";
 import { scaleLinear } from "d3-scale";
-import type { DSVRowArray } from "d3";
+import { geoCentroid, GeoGeometryObjects, type DSVRowArray } from "d3";
 import {
   ComposableMap,
   Geographies,
   Geography,
   Sphere,
   Graticule,
+  ZoomableGroup,
 } from "react-simple-maps";
+import { Slider } from "../ui/slider";
 
 const geoUrl = "/features.json";
 
@@ -16,8 +18,18 @@ const colorScale = scaleLinear<string>()
   .domain([0.29, 0.68])
   .range(["#ffedea", "#ff5233"]);
 
-const MapChart = () => {
+const MapChart = ({
+  selectedCountry,
+  setSelectedCountry,
+}: {
+  selectedCountry: string;
+  setSelectedCountry: Function;
+}) => {
   const [data, setData] = useState<DSVRowArray<string> | null>(null);
+  const [camera, setCamera] = useState<{
+    center: [number, number];
+    zoom: number;
+  }>({ center: [0, 0], zoom: 1 });
 
   useEffect(() => {
     csv(`/vulnerability.csv`).then((data) => {
@@ -25,37 +37,98 @@ const MapChart = () => {
     });
   }, []);
 
+  const handleCountryClick = (geo: GeoGeometryObjects) => {
+    console.log(geo);
+    setSelectedCountry(geo.properties.name);
+    const centroid = geoCentroid(geo);
+    setCamera({ center: centroid, zoom: 4 });
+  };
+
+  const handleFilter = (filter: SVGElement) => {
+    return filter.type !== "dblclick";
+  };
+
   return (
-    <ComposableMap
-      projectionConfig={{
-        rotate: [-10, 0, 0],
-        scale: 147,
-      }}
-    >
-      <Sphere stroke="#E4E5E6" strokeWidth={0.5} id={""} fill={"transparent"} />
-      <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
-      {data && data.length > 0 && (
-        <Geographies geography={geoUrl}>
-          {({ geographies }) =>
-            geographies.map((geo) => {
-              const d = data.find((s) => s.ISO3 === geo.id);
-              return (
-                <Geography
-                  key={geo.rsmKey}
-                  geography={geo}
-                  fill={d ? colorScale(Number(d["2017"])) : "#F5F4F6"}
-                  style={{
-                    default: { outline: "none" },
-                    hover: { outline: "none" },
-                    pressed: { outline: "none" },
-                  }}
-                />
-              );
-            })
-          }
-        </Geographies>
-      )}
-    </ComposableMap>
+    <>
+      <ComposableMap
+        projectionConfig={{
+          rotate: [-10, 0, 0],
+          scale: 147,
+        }}
+        height={450}
+        style={{ width: "100%", height: "auto" }}
+      >
+        <ZoomableGroup
+          center={camera.center}
+          zoom={camera.zoom}
+          onMoveEnd={(position) => {
+            setCamera({
+              center: [position.coordinates[0], position.coordinates[1]],
+              zoom: position.zoom,
+            });
+          }}
+          filterZoomEvent={handleFilter}
+        >
+          <Sphere
+            stroke="#E4E5E6"
+            strokeWidth={0.5}
+            id={""}
+            fill={"transparent"}
+          />
+          <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+
+          {data && data.length > 0 && (
+            <Geographies geography={geoUrl}>
+              {({ geographies }) => {
+                const reorderedGeographies = [...geographies];
+                const selectedIndex = reorderedGeographies.findIndex(
+                  (geo) => geo.properties.name === selectedCountry
+                );
+                if (selectedIndex !== -1) {
+                  const [selectedGeo] = reorderedGeographies.splice(
+                    selectedIndex,
+                    1
+                  );
+                  reorderedGeographies.push(selectedGeo);
+                }
+                return reorderedGeographies.map((geo) => {
+                  const d = data.find((s) => s.ISO3 === geo.id);
+                  return (
+                    <Geography
+                      key={geo.rsmKey}
+                      geography={geo}
+                      fill={d ? colorScale(Number(d["2017"])) : "#F5F4F6"}
+                      stroke={
+                        geo.properties.name === selectedCountry
+                          ? "#000000"
+                          : "#FFFFFF"
+                      }
+                      strokeWidth={
+                        geo.properties.name === selectedCountry ? 2 : 1
+                      }
+                      style={{
+                        default: { outline: "none" },
+                        hover: {
+                          outline: "none",
+                          // strokeWidth: 4,
+                          // stroke: "#607D8B",
+                          // zIndex: 1000,
+                        },
+                        pressed: {
+                          outline: "none",
+                        },
+                      }}
+                      onClick={() => handleCountryClick(geo)}
+                      vectorEffect="non-scaling-stroke"
+                    />
+                  );
+                });
+              }}
+            </Geographies>
+          )}
+        </ZoomableGroup>
+      </ComposableMap>
+    </>
   );
 };
 
