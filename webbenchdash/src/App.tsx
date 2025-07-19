eb4b3b8840af04a97b5a7389ff212a407148ec46
3LucasZ <lucas.zheng@warriorlife.net>
2025-07-19 16:51:34 -0700

    dark mode

--- webbenchdash/src/App.tsx
+++ webbenchdash/src/App.tsx
@@ -5,6 +5,7 @@ import { ClusterChart } from "./components/macro/cluster-chart";
 import { PercentileChart } from "./components/macro/percentile-chart";
 import { ResilienceDNSRaw } from "./components/macro/resilience-dns-raw";
 import { CountryName } from "./components/macro/country-name";
+import { ThemeProvider } from "./components/theme-provider";
 
 function App() {
   const [selectedCountry, setSelectedCountry] = useState<string>("");
@@ -13,57 +14,59 @@ function App() {
   const [selectedSubset, setSelectedSubset] = useState("gov");
 
   return (
-    <div className="flex flex-col h-screen min-h-screen overflow-hidden">
-      <p className="text-center text-4xl font-medium py-4">
-        WebBench Dashboard
-      </p>
-      {/* <div className="min-h-4"></div> */}
-      <div className="w-full flex justify-center">
-        <FeatureSelect
-          value1={selectedFeature}
-          onValue1Change={setSelectedFeature}
-          value2={selectedSubset}
-          onValue2Change={setSelectedSubset}
-        />
-      </div>
-      <div className="min-h-4"></div>
-      <CountryName c1={selectedCountry} c2={selectedCountry2} />
-      <div className="min-h-4"></div>
-      <MapChart
-        selectedCountry={selectedCountry}
-        setSelectedCountry={setSelectedCountry}
-        selectedCountry2={selectedCountry2}
-        setSelectedCountry2={setSelectedCountry2}
-      />
-      <div className="flex max-w-6xl gap-4 px-4 bottom-3 sticky">
-        {selectedFeature == "reliability" && (
-          <div className="w-1/2">
-            <ResilienceDNSRaw
-              country={selectedCountry}
-              subset={selectedSubset}
-            />
-          </div>
-        )}
-        {selectedFeature == "reliability" && selectedCountry2 && (
-          <div className="w-1/2">
-            <ResilienceDNSRaw
-              country={selectedCountry2}
-              subset={selectedSubset}
-            />
-          </div>
-        )}
-        {selectedFeature == "performance" && (
-          <div className="w-1/2">
-            <ClusterChart country={selectedCountry} />
-          </div>
-        )}
-
-        <div className="w-1/2">
-          <PercentileChart country={selectedCountry} />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col h-screen min-h-screen overflow-hidden">
+        <p className="text-center text-4xl font-medium py-4">
+          WebBench Dashboard
+        </p>
+        {/* <div className="min-h-4"></div> */}
+        <div className="w-full flex justify-center">
+          <FeatureSelect
+            value1={selectedFeature}
+            onValue1Change={setSelectedFeature}
+            value2={selectedSubset}
+            onValue2Change={setSelectedSubset}
+          />
         </div>
-        {/* <div className="w-1/3"><SecurityChart /></div> */}
+        <div className="min-h-4"></div>
+        <CountryName c1={selectedCountry} c2={selectedCountry2} />
+        <div className="min-h-4"></div>
+        <MapChart
+          selectedCountry={selectedCountry}
+          setSelectedCountry={setSelectedCountry}
+          selectedCountry2={selectedCountry2}
+          setSelectedCountry2={setSelectedCountry2}
+        />
+        <div className="flex max-w-6xl gap-4 px-4 bottom-3 sticky">
+          {selectedFeature == "reliability" && (
+            <div className="w-1/2">
+              <ResilienceDNSRaw
+                country={selectedCountry}
+                subset={selectedSubset}
+              />
+            </div>
+          )}
+          {selectedFeature == "reliability" && selectedCountry2 && (
+            <div className="w-1/2">
+              <ResilienceDNSRaw
+                country={selectedCountry2}
+                subset={selectedSubset}
+              />
+            </div>
+          )}
+          {selectedFeature == "performance" && (
+            <div className="w-1/2">
+              <ClusterChart country={selectedCountry} />
+            </div>
+          )}
+
+          <div className="w-1/2">
+            <PercentileChart country={selectedCountry} />
+          </div>
+          {/* <div className="w-1/3"><SecurityChart /></div> */}
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 }
 
