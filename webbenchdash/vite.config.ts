008e9b3a3a952f9d313b04b654e15bb278c54265
3LucasZ <lucas.zheng@warriorlife.net>
2025-09-04 02:06:13 -0700

    Update vite.config.ts

--- webbenchdash/vite.config.ts
+++ webbenchdash/vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/webbenchdash_test",
+  base: "/WebBenchDash_Test",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
